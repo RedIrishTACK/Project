040268b1ef59bbc8d8a3b4a0d961b0e3e801b6b8
RedIrishTACK <anthonygildhouse@yahoo.com>
2015-11-10 19:23:45 -0600

    Project
    Project

---
+++ week four GLD Anthony Gildhouse/Assets/scripts/Instantiate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instantiate : MonoBehaviour {
+	
+	public GameObject bullet;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Fire1")) {
+			Shoot();
+		}
+		
+	}
+	
+	void Shoot() {
+		Rigidbody clone;        // creating new variable of type Rigidbody.
+		clone = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+		clone.transform.Translate(1, 1, 0);         // make the projectile spawn in the right location (inside the gun barrel for instance.) BUT make sure it is outside of the player or it will immediately kill itself.
+		clone.velocity = transform.TransformDirection(Vector3.forward * 50);       // the direction the projectile goes.
+	}
+}
---
+++ week four GLD Anthony Gildhouse/Assets/scripts/TeleportDoor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportDoor : MonoBehaviour {
+	
+	public GameObject destination, player;
+	public bool inDoorway = false;
+	
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.E) && inDoorway) {
+			player.transform.position = destination.transform.position;
+		}
+	}
+	
+	void OnGUI() {
+		if(inDoorway) {
+			GUI.Box(new Rect(Screen.width/2,Screen.height/2, 75,25), "Press 'E'");
+		}
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			inDoorway = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			inDoorway = false;
+		}
+	}
+	
+	
+}
